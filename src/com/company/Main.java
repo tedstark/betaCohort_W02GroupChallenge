dc1ee988f32d2d9cb7b78a67383fc1e3538522ae
Ted <ted.stark@gmail.com>
2018-01-19 07:21:55 -0700

    Update #1: reorganize & random

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -7,6 +9,7 @@ public class Main {
     private static int boardWidth = 5;
     private static int boardHeight = 5;
     private static String [][] gameBoard = new String[boardWidth][boardHeight];  //Declare Game Board Array
+    private static List<String> shipArray2 = new ArrayList<>(5);
 
     public static void main(String[] args) {
         /* Group Participation #2 – Week 2
@@ -26,9 +29,17 @@ public class Main {
                     •    Process arrays using loops
                     •    Create and utilize 2 dimensional arrays to model tables and grids */
 
+        //| 0,0 | 1,0 | 2,0 | 3,0 | 4,0 |
+        //| 0,1 | 1,1 | 2,1 | 3,1 | 4,1 |
+        //| 0,2 | 1,2 | 2,2 | 3,2 | 4,2 |
+        //| 0,3 | 1,3 | 2,3 | 3,3 | 4,3 |
+        //| 0,4 | 1,4 | 2,4 | 3,4 | 4,4 |
+
+
         //welcome & decision block
+        System.out.println(""); //Blank Line
         System.out.println("Welcome to Battleship Limited! This is a modified, simplified game for one person across a 5x5 game board. ");
-        System.out.println("Would you like to play? (Yes/No)");
+        System.out.println("Would you like to play with me? (Yes/No)");
         Scanner sc = new Scanner(System.in);
         //String playGame = sc.nextLine();
 
@@ -36,13 +47,28 @@ public class Main {
         //while (playGame.equalsIgnoreCase("Yes")) {
             System.out.println("Great! Let me get the game board ready....");
 
-            //code for randomized ship location selection
-            Random shipSelection = new Random();
-            int shipRandomX = shipSelection.nextInt(boardWidth);
-            int shipRandomY = shipSelection.nextInt(boardHeight);
-            System.out.println("Random Ship Selection is: " + shipRandomX + "," + shipRandomY);
+        //Code block for randomized ship location selection
+        Random shipSelection = new Random();
+        //int shipArrayPos = 0;
+        for (int i = 0; i < 5; i++) {
+            int shipTempX = shipSelection.nextInt(boardWidth);
+            int shipTempY = shipSelection.nextInt(boardHeight);
+            String shipLocation = (Integer.toString(shipTempX) + (Integer.toString(shipTempY)));
+            //System.out.println("Random Ship Selection is: " + shipTempX + "," + shipTempY);
+            while (shipArray2.contains(shipLocation)) {
+                //System.out.println("Duplicate Random");
+                shipTempX = shipSelection.nextInt(boardWidth);
+                shipTempY = shipSelection.nextInt(boardHeight);
+                shipLocation = (Integer.toString(shipTempX) + (Integer.toString(shipTempY)));
+            }
+            shipArray2.add(shipLocation);
+        }
+        //code to display ship location array for troubleshooting purposes
+        System.out.println("");
+        System.out.println("Here are my locations:  " + shipArray2);
 
-            //Code block to assign gameBoard array with values:
+
+        //code to assign game array with values:
             gameBoard [0][0]= "A1";
             gameBoard [1][0]= "A2";
             gameBoard [2][0]= "A3";
@@ -75,7 +101,7 @@ public class Main {
             System.out.println("Here's your game board:");
             System.out.println("----------------");
             printGameBoard(); //calls method "printGameBoard" to print game board for user on screen
-            //System.out.println("Select the coordinates of where a ship might be:");
+            System.out.println("Select the coordinates of where a ship might be:");
             //String userGuess = sc.nextLine();
 
             //code to Play Again or quit decision
@@ -85,10 +111,13 @@ public class Main {
         //}
         // MAIN WHILE LOOP END
         System.out.println("Okay, have a nice day.");
+
+        // CODE Exit
     }
-    public static void printGameBoard(){
-        /*Exterior*/for (int y = 0; y < boardHeight; y++) {
-            /*Interior*/for (int x = 0; x < boardWidth; x++) {
+    //Method to print the updated game board.
+    public static void printGameBoard() {
+        for (int y = 0; y < boardHeight; y++) { /*Exterior loop*/
+            for (int x = 0; x < boardWidth; x++) { /*Interior loop*/
                 System.out.print(gameBoard[x][y] + " ");
             }
             System.out.println("");
